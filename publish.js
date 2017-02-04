a307ab7bbb60282ffd20fe9a0a409513ada5d077
mlxiao <mlxiao93@163.com>
2017-02-04 18:07:45 +0800

    modify config

--- publish.js
+++ publish.js
@@ -1,7 +1,7 @@
 var ghpages = require('gh-pages');
 var path = require('path');
 
-ghpages.publish(path.join(__dirname, 'dist'), function(err) {
+ghpages.publish(path.join(__dirname, 'public'), function(err) {
   if (err) return console.log(err);
   console.log('publish success');
 });
---
+++ source/_posts/js-extend.md
@@ -0,0 +1,6 @@
+---
+title: javascript继承
+date: 2017-02-04 16:15:40
+tags:
+ - javascript
+---
