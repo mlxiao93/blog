5132416fe47b4f275b00a2935e16a4e02b00fd8a
m.l.xiao <edz@edzdeMacBook-Pro.local>
2017-02-05 17:28:02 +0800

    make activated menu styled

--- source/about/index.md
+++ source/about/index.md
@@ -3,3 +3,10 @@ title: about
 layout: page
 comments: false
 ---
+
+<div style="text-align: center">![avatar](http://o86lf0oxm.bkt.clouddn.com/jiujiu.jpg)</div>
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;IT男，码农，15年毕业，现居上海，志(无)在(以)四(为)方(家)，前端开发从业者。写此博客主要为了记录开发中遇到和总结的问题，以作备忘，若碰巧能帮到他人那将不甚荣幸。
+<div style="text-align: right; margin-top: -16px"> *contact me @ mlxiao93@163.com*</div>
+
+> 友情链接：<a href="https://mlxiao93.github.io/demos" target="_blank">https://mlxiao93.github.io/demos</a>
--- themes/even/source/js/src/bootstrap.js
+++ themes/even/source/js/src/bootstrap.js
@@ -4,4 +4,16 @@ $(document).ready(function () {
   Even.search.register();
   Even.postToc.register();
   Even.backToTop.register();
+
+  var path = location.pathname;
+  var menuItems = $('.site-nav .menu-item');
+  if (/blog\/$/i.test(path)) {
+    $(menuItems[0]).addClass('active');
+  } else if (/archives\/$/i.test(path)) {
+    $(menuItems[1]).addClass('active');
+  } else if (/tags\/$/i.test(path)) {
+    $(menuItems[2]).addClass('active');
+  } else if (/about\/$/i.test(path)) {
+    $(menuItems[3]).addClass('active');
+  }
 })
