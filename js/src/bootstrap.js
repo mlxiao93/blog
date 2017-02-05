406155fe3ec3ac4b255af50c0a87fb0e84405aea
m.l.xiao <edz@edzdeMacBook-Pro.local>
2017-02-05 17:28:32 +0800

    Updates

--- js/src/bootstrap.js
+++ js/src/bootstrap.js
@@ -1,8 +1,19 @@
 $(document).ready(function () {
-  console.log('dom ready');
   Even.fancybox.register();
   Even.mobileNavbar.register();
   Even.search.register();
   Even.postToc.register();
   Even.backToTop.register();
+
+  var path = location.pathname;
+  var menuItems = $('.site-nav .menu-item');
+  if (/blog\/$/i.test(path)) {
+    $(menuItems[0]).addClass('active');
+  } else if (/archives\/$/i.test(path)) {
+    $(menuItems[1]).addClass('active');
+  } else if (/tags\/$/i.test(path)) {
+    $(menuItems[2]).addClass('active');
+  } else if (/about\/$/i.test(path)) {
+    $(menuItems[3]).addClass('active');
+  }
 })
